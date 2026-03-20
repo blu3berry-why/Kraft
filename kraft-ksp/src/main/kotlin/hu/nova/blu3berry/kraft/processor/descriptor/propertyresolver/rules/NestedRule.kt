69a3579a96dd06eea7487a80326f1bb2fb06a698
gyenese.matyas <gyenese.matyas@novaservices.hu>
2026-03-20 15:25:43 +0100

    Remove the misleading warning from NestedRule

--- kraft-ksp/src/main/kotlin/hu/nova/blu3berry/kraft/processor/descriptor/propertyresolver/rules/NestedRule.kt
+++ kraft-ksp/src/main/kotlin/hu/nova/blu3berry/kraft/processor/descriptor/propertyresolver/rules/NestedRule.kt
@@ -20,29 +20,6 @@ class NestedRule : MappingRule {
             prop.type.className == nested.sourceType.className
         } ?: return null
 
-        ctx.logger.warn("""
-    ****************************************************
-    *                  ⚠  WARNING                      *
-    ****************************************************
-    
-    The current version of the AutoMapper does NOT
-    generate nested mappings.
-    
-    Please ensure that a mapper between the following
-    types exists:
-    
-        * ${sourceProp.type.className} -> ${target.type.className}
-    
-    If no mapping exists, you can add the appropriate
-    annotations to the target object.
-    The mapping will then be generated automatically.
-    
-    Without this mapping, nested objects will NOT
-    be mapped correctly.
-    
-    ****************************************************
-        """)
-
         return PropertyMappingStrategy.NestedMapper(
             targetProperty = target,
             sourceProperty = sourceProp,
