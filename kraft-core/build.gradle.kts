4c0838ab684bd7a973ea576d239363116eb76d95
gyenese.matyas <gyenese.matyas@novaservices.hu>
2026-03-28 11:44:54 +0100

    feat(ci): add Maven Central publishing with staging and fix   release-branch conflicts

--- kraft-core/build.gradle.kts
+++ kraft-core/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     id("maven-publish")
+    signing
 }
 
 kotlin {
@@ -23,6 +24,10 @@ kotlin {
     }
 }
 
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
 publishing {
     publications {
         withType<MavenPublication> {
@@ -34,6 +39,32 @@ publishing {
             groupId = "com.blu3berry.kraft"
             artifactId = "kraft-core"
             version = (project.properties["kraft.version"] as? String) ?: "0.0.0-SNAPSHOT"
+
+            artifact(javadocJar)
+
+            pom {
+                name.set("kraft-core")
+                description.set("Core processor model and SPI for the Kraft mapper generator")
+                url.set("https://github.com/blu3berry-why/Kraft")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("blu3berry-why")
+                        name.set("blu3berry")
+                        url.set("https://github.com/blu3berry-why")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/blu3berry-why/Kraft")
+                    connection.set("scm:git:git://github.com/blu3berry-why/Kraft.git")
+                    developerConnection.set("scm:git:ssh://github.com/blu3berry-why/Kraft.git")
+                }
+            }
         }
     }
 
@@ -47,5 +78,24 @@ publishing {
                 password = project.findProperty("gpr.token") as String?
             }
         }
+        maven {
+            name = "MavenCentral"
+            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = findProperty("maven.central.username") as String?
+                password = findProperty("maven.central.password") as String?
+            }
+        }
     }
+}
+
+signing {
+    isRequired = findProperty("signing.key") != null
+    useInMemoryPgpKeys(
+        findProperty("signing.keyId") as String?,
+        findProperty("signing.key") as String?,
+        findProperty("signing.password") as String?
+    )
+    sign(publishing.publications)
 }
